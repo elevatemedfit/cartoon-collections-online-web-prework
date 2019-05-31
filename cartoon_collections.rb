896bfcc998fec47bfc81003d3a0df2fab9a72ced
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-31 13:58:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index do |name|
     puts name# Your code here
 end
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
